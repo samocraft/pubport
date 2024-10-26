ffa96de0e656425169ea4ef63e37b9b85fbc3419
samocraft <57090135+samocraft@users.noreply.github.com>
2024-10-26 13:04:03 +0100

    commitconflicted

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 
 
@@ -75,7 +75,43 @@ for (let i = 0; i < selectItems.length; i++) {
   });
 }
 
+// filter variables
+const filterItems = document.querySelectorAll("[data-filter-item]");
 
+const filterFunc = function (selectedValue) {
+
+  for (let i = 0; i < filterItems.length; i++) {
+
+    if (selectedValue === "all") {
+      filterItems[i].classList.add("active");
+    } else if (selectedValue === filterItems[i].dataset.category) {
+      filterItems[i].classList.add("active");
+    } else {
+      filterItems[i].classList.remove("active");
+    }
+
+  }
+
+}
+
+// add event in all filter button items for large screen
+let lastClickedBtn = filterBtn[0];
+
+for (let i = 0; i < filterBtn.length; i++) {
+
+  filterBtn[i].addEventListener("click", function () {
+
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    filterFunc(selectedValue);
+
+    lastClickedBtn.classList.remove("active");
+    this.classList.add("active");
+    lastClickedBtn = this;
+
+  });
+
+}
 
 
 
