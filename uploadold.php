f1016b34e147c770d206475d8a88574d3d26cf55
samocraft <57090135+samocraft@users.noreply.github.com>
2024-10-26 13:08:13 +0100

    commitconflictedagainagain

--- uploadold.php
+++ uploadold.php
@@ -9,6 +9,7 @@ require_once 'connection.php';
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <<<<<<< HEAD
+<<<<<<< HEAD
     if (isset($_FILES["files"]) && isset($_FILES["thumbnail"])) { // Check if files are uploaded
         $title = $_POST['title'];
         $subtitle = $_POST['subtitle'];
@@ -103,6 +104,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if($check !== false) {
         $uploadOk = 1;
     } else {
+=======
+    $title = $_POST['title'];
+    $subtitle = $_POST['subtitle'];
+    $category = $_POST['category'];
+
+    // File upload handling
+    $target_dir = "uploads/"; // Directory where uploaded files will be stored
+    $target_file = $target_dir . basename($_FILES["picture"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+    // Check if image file is a actual image or fake image
+    $check = getimagesize($_FILES["picture"]["tmp_name"]);
+    if($check !== false) {
+        $uploadOk = 1;
+    } else {
+>>>>>>> parent of 0140e45 (video view/upload lts)
         echo "File is not an image.";
         $uploadOk = 0;
     }
@@ -154,6 +172,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         } else {
             echo "Sorry, there was an error uploading your file.";
         }
+<<<<<<< HEAD
+>>>>>>> parent of 0140e45 (video view/upload lts)
+=======
 >>>>>>> parent of 0140e45 (video view/upload lts)
     }
 }
